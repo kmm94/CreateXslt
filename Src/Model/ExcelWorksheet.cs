118117d3473bd30fb43e3f51ec50668faf7d8e91
Joachim Hejlesen <joachim-dk@hotmail.com>
2024-02-26 22:29:33 +0100

    Cleaned up project to use .NET Core 8 instead of .NET Framework.

---
+++ Src/Model/ExcelWorksheet.cs
@@ -0,0 +1,21 @@
+﻿namespace CreateXslt
+{
+    public class ExcelWorksheet
+    {
+        public List<Column> columns { get; set; }
+        public string name { get; set; }
+        
+        public string specialFirstLine { get; set; }
+
+        public ExcelWorksheet(List<Column> columns, string name = "Data")
+        {
+            this.columns = columns;
+            this.name = name;
+        }
+
+        public ExcelWorksheet(List<Column> tableColumns)
+        {
+            this.columns = tableColumns;
+        }
+    }
+}
